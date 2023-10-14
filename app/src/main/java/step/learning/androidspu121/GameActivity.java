ff845b7d31dfe8b7e85a2acdf763911e20626ce6
Denys Samoilenko <denniksam@gmail.com>
2023-10-14 13:15:09 +0300

    2048 -- 2

--- app/src/main/java/step/learning/androidspu121/GameActivity.java
+++ app/src/main/java/step/learning/androidspu121/GameActivity.java
@@ -3,6 +3,8 @@ package step.learning.androidspu121;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
@@ -11,6 +13,26 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_game );
+
+        // Задати ігровому полю висоту таку ж як ширину
+        // Проблема: на етапі onCreate розміри ще не відомі
+        TableLayout gameField = findViewById( R.id.game_field ) ;
+        gameField.post(   // поставити задачу у чергу, вона буде виконана
+                // коли gameField виконає усі попередні задачі, у т.ч.
+                // розрахунок розміру та рисування.
+                () -> {
+                    int windowWidth = this.getWindow().getDecorView().getWidth() ;
+                    int margin =
+                        ((LinearLayout.LayoutParams)gameField.getLayoutParams()).leftMargin;
+                    int fieldSize = windowWidth - 2 * margin ;
+                    LinearLayout.LayoutParams layoutParams =
+                            new LinearLayout.LayoutParams( fieldSize, fieldSize ) ;
+                    layoutParams.setMargins( margin, margin, margin, margin );
+                    gameField.setLayoutParams( layoutParams ) ;
+                }
+        ) ;
+
+
         findViewById( R.id.game_layout ).setOnTouchListener(
                 new OnSwipeListener( GameActivity.this ) {
                     @Override public void onSwipeBottom() {
@@ -32,3 +54,8 @@ public class GameActivity extends AppCompatActivity {
                 } ) ;
     }
 }
+/*
+Д.З. Підібрати кольори для оформлення всіх станів гри (2048)
+Реалізувати їх ресурси за єдиним правилом "префікс_{значення}"
+Випробувати через впровадження стилів
+ */
